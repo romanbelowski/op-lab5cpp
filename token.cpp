0d4177a27e66dd53e705d7d3e4ac10a0d7ef1c7c
Dmytro Herashchenko <51529317+dmytro-duck@users.noreply.github.com>
2021-03-14 11:29:31 +0200

    Add half-worked Token Class

---
+++ token.cpp
@@ -0,0 +1,42 @@
+#include "token.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+string OPERATORS = "+-*/";
+
+Token::Token(string value) {
+  token = value;
+}
+
+bool Token::IsOperator(string str) {
+  if (OPERATORS.find(str) != string::npos)
+    return true;
+  return false;
+}
+
+bool Token::GetIsOperator() {
+  return IsOperator(token);
+}
+
+int Token::GetPrecedence() {
+  if (token == "*" || token == "/")
+    return 3;
+  if (token == "+" || token == "-")
+    return 2;
+  return 0;
+}
+
+float Token::Calculate(float x, float y) {
+  if (GetIsOperator()) {
+    if (token == "+")
+      return x + y;
+    if (token == "-")
+      return x - y;
+    if (token == "*")
+      return x * y;
+    if (token == "/")
+      return x / y;
+  }
+}
---
+++ token.hpp
@@ -0,0 +1,15 @@
+#include <string>
+
+class Token {
+ public:
+  bool IsOperator(std::string str);
+  Token(std::string str);
+  bool GetIsOperator();
+  int GetPrecedence();
+  float GetValue();
+  bool IsValue();
+  float Calculate(float, float);
+
+ private:
+  std::string token;
+};
