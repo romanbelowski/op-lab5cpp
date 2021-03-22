e99efb42a964975bf18f7b10d1389d65cc6185ed
Dmytro Herashchenko <51529317+dmytro-duck@users.noreply.github.com>
2021-03-22 03:34:29 +0200

    Add: CLI input and split

--- main.cpp
+++ main.cpp
@@ -3,12 +3,29 @@
 #include <iostream>
 #include <string>
 
-#include "calculate.hpp"
+// #include "calculate.hpp"
 #include "queue.hpp"
-#include "stack.hpp"
 #include "token.hpp"
+
 using namespace std;
 
+Queue<Token> SplitExpression(string);
+
+int main(int argc, char* argv[]) {
+  string expression = argv[1];
+  Queue<Token> queue = SplitExpression(expression);
+
+  queue.display();
+
+  // Queue<Token*> res = ShuntingYard(queue);
+  // res.display();
+
+  // cout << CalculateRPN(res) << endl;
+
+  return 0;
+}
+
+// Видалає пропуски з рядка
 void RemoveSpaces(string& str) {
   size_t pos = str.find(' ');
   while (pos != string::npos) {
@@ -17,39 +34,48 @@ void RemoveSpaces(string& str) {
   }
 }
 
-int main(int argc, char* argv[]) {
-  // string expression;
-  // expression = argv[1];
-  // cout << "-------" << endl;
-  // cout << expression << endl;
-  // RemoveSpaces(expression);
-  // cout << expression << endl;
-  // cout << OPERATORS << endl;
+// Повертає позицію і розмір наступного оператора
+size_t next_op(string str, size_t start_pos, size_t* size) {
+  size_t operator_pos = str.find_first_of(OPERATORS, start_pos),
+         function_pos = str.find("sqrt", start_pos),
+         result_pos = min(operator_pos, function_pos);
 
-  OperatorToken plus = OperatorToken("+");
-  OperatorToken minus = OperatorToken("-");
-  OperatorToken times = OperatorToken("*");
-  ValueToken four = ValueToken("4");
-  ValueToken five = ValueToken("5");
-  ValueToken six = ValueToken("6");
-  ValueToken seven = ValueToken("7");
+  if (result_pos == string::npos) {
+    *size = string::npos;
+  } else if (result_pos == operator_pos) {
+    *size = 1;
+  } else if (result_pos == function_pos) {
+    *size = 4;
+  }
+  return result_pos;
+}
 
-  // Stack<Token*> golang(16);
-  Queue<Token*> queue(16);
-  queue.enqueue(&four);
-  queue.enqueue(&plus);
-  queue.enqueue(&five);
-  queue.enqueue(&minus);
-  queue.enqueue(&six);
-  queue.enqueue(&times);
-  queue.enqueue(&seven);
+// Роздляє рядок на чергу токенів
+Queue<Token> SplitExpression(string str) {
+  RemoveSpaces(str);
+  Queue<Token> out(16);
+  string token;
+  size_t start_pos = 0, op_size, op_pos = next_op(str, start_pos, &op_size);
+  while (op_pos != string::npos) {
+    // Число між операторами
+    if (start_pos < op_pos) {
+      token = str.substr(start_pos, op_pos - start_pos);
+      out.enqueue(Token(token));
+    }
 
-  queue.display();
+    // Оператор
+    token = str.substr(op_pos, op_size);
+    out.enqueue(Token(token));
 
-  Queue<Token*> res = ShuntingYard(queue);
-  res.display();
+    start_pos = op_pos + op_size;
+    op_pos = next_op(str, start_pos, &op_size);
+  }
 
-  cout << CalculateRPN(res) << endl;
+  // Число в кінці
+  if (start_pos < str.size()) {
+    token = str.substr(start_pos, string::npos);
+    out.enqueue(Token(token));
+  }
 
-  return 0;
+  return out;
 }
